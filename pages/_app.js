e4ff5d81461a34d27cfe65be49f50089821a5757
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-01-28 08:09:15 +0000

    Sent Toast #hello

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,7 @@ const Msg = ({ resumeId }) => {
     );
     localStorage.removeItem("persist:root");
     toast.info(<Msg resumeId={resumeId.data} />);
+    console.log('Sent Toast')
     if (resumeId) if (resumeId) toast.info(<Msg resumeId={resumeId.data} />);
     countapi.update("animex.ninja", process.env.key, 1).then((result) => {
       setVisit(result.value);
