7c40c02aaf43ee86377805646a718990bcc4465b
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-15 17:44:33 +0530

    Update help.js

--- pages/help.js
+++ pages/help.js
@@ -7,7 +7,8 @@ const help = () => {
     <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
   <div class="pt-6 space-y-4">
     <blockquote>
-      <p class="text-lg font-medium">
+      <p class={`${theme.text.selected} block py-2 pl-3 pr-4 text-white-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
+      >
         “Try using VPN to watch videos !!”
       </p>
     </blockquote>
@@ -23,3 +24,4 @@ const help = () => {
 };
 
 export default help;
+// class={`${theme.text.selected} block py-2 pl-3 pr-4 text-white-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
