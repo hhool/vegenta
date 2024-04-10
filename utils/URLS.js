a47bb27580f608934c073e19d541c37c31d4257d
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-10 17:05:16 +0530

    Update URLS.js

--- utils/URLS.js
+++ utils/URLS.js
@@ -1,6 +1,6 @@
 export const URL = {
   POPULAR: "https://tes-henna.vercel.app/api/popular/",
-  RECENT: "https://tes-henna.vercel.app/api/recentlyadded/",
+  RECENT: "https://animetize-api.vercel.app/anime/gogoanime/recent-episodes/",
   DETAILS: "https://tes-henna.vercel.app/api/details/",
   EPLINK: "https://tes-henna.vercel.app/api/watching/",
   GENRES: "https://tes-henna.vercel.app/api/genre/",
