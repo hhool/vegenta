0269c3043f0504af5760eb34fc1eb6b77e98ccb4
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-10 17:50:11 +0530

    Create watchingDataAction.js

---
+++ redux/actions/watchingDataAction.js
@@ -0,0 +1,21 @@
+import { MyAction } from "./actions";
+
+const fetchData = () => {
+  return { type: MyAction.FETCH };
+};
+const recieveData = (payload) => {
+  return { type: MyAction.RECEIVE, payload: payload };
+};
+
+export const asyncDataAction = (URL) => {
+  return async function (dispatch) {
+    dispatch(fetchData());
+    console.log(URL);
+    const response = await fetch(URL);
+    const res = await response.json();
+    const gogoServer = response.data[1];
+    console.log(gogoServer.name);
+    const result = gogoServer.url);
+    dispatch(recieveData(result));
+  };
+};
