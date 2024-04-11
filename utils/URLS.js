5c75e9e5254b518399103bbc6041f940e1a2813e
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-11 11:56:41 +0530

    Update URLS.js

--- utils/URLS.js
+++ utils/URLS.js
@@ -1,8 +1,8 @@
 export const URL = {
   POPULAR: "https://animetize-api.vercel.app/anime/gogoanime/top-airing?page=", //fixed
   RECENT: "https://animetize-api.vercel.app/anime/gogoanime/recent-episodes?page=", //fixed
-  DETAILS: "https://tes-henna.vercel.app/api/details/", //fixed
+  DETAILS: "https://vegenta-api.vercel.app/api/details/", //fixed
   EPLINK: "https://animetize-api.vercel.app/anime/gogoanime/servers/", // almost done --
-  GENRES: "https://tes-henna.vercel.app/api/genre/", //usless so no fix
-  SEARCH: "https://tes-henna.vercel.app/api/search/", //fixed
+  GENRES: "https://vegenta-api.vercel.app/api/genre/", //usless so no fix
+  SEARCH: "https://vegenta-api.vercel.app/api/search/", //fixed
 };
